d91e113d4df6567acd3625b6306418174aedb247
SlyAsFox <slyasfox7@gmail.com>
2019-01-30 15:43:59 +0200

    game without random generation a Secret number

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+function generateSecretNumber() {
+
+    return '2578';
+}
+
+function validateNumber(number){
+
+    if(typeof(number) === 'number'){
+        let str = String(number);
+    }
+    for(let i = 0; i < str.length; i++){
+        let counter = 0;
+        for(let j = 0; j < str.length; j++) {
+            if(str[i] === str[j]){
+                counter++;
+            }
+        }
+        if (counter > 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function getBullsCount(numberString){
+
+    let counter = 0;
+    for(let i = 0; i < numberString.length; i++){
+        if (numberString[i] === secretNumber[i]){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+function getCowsCount(numberString){
+    let counter = 0;
+    for (let i = 0; i < numberString.length; i++ ){
+        let position = secretNumber.indexOf(numberString[i]);
+        if (position !== -1 && position !== i){
+
+            counter++;
+        }
+    }
+    return counter;
+}
+
+
+const secretNumber = generateSecretNumber()
+let prevResults = '';
+
+let inputNumber = prompt('Input your number:');
+
+while(inputNumber){
+    let bullsCount = getBullsCount(inputNumber);
+    let cowsCount = getCowsCount(inputNumber);
+    if (bullsCount === 4){
+        alert(`You win!!! Secret number - ${inputNumber}`);
+        break;
+    }
+    prevResults += `Your number: ${inputNumber}   bulls: ${bullsCount}   cows: ${cowsCount} \n`
+    inputNumber = prompt(prevResults);
+
+}
